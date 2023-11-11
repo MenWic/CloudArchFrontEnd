ccda535620eef50bb3547a39d75b10d9b47bad33
MenWic <lamr467@gmail.com>
2023-11-11 01:12:08 -0600

    Redirecciones en Navbar correctas

--- src/app/models/admin/admin-top-navbar/admin-top-navbar.component.ts
+++ src/app/models/admin/admin-top-navbar/admin-top-navbar.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-admin-top-navbar',
   templateUrl: './admin-top-navbar.component.html',
-  styleUrls: ['./admin-top-navbar.component.css']
+  styleUrls: ['./admin-top-navbar.component.css'],
 })
 export class AdminTopNavbarComponent {
+  //Constructor
+  constructor(
+    private router: Router, 
+    private cookieService: CookieService
+    ) {}
 
+  public logout() {
+    this.cookieService.delete('usuario');
+    this.router.navigate(['login']);
+  }
 }
